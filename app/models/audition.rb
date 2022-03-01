747858aae27165e70056dd5922372137127c22d2
Kevin Gallo <Gallok731@gmail.com>
2022-03-01 10:07:15 -0500

    commit tree d9ac0b78f69daa4425afd4a8ee36c22e8b53b8d3

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,3 +1,8 @@
 class Audition < ActiveRecord::Base
     belongs_to :role
+
+    def self.role
+        return self.role
+    end
+
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,5 @@
 class Role < ActiveRecord::Base
-    has_many :Audition 
+    has_many :audition 
 
 end
 
